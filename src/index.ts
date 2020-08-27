6bb2252f83d80f5336e837f90a72b196a469464b
Hermetheus <allanmolson@gmail.com>
2020-08-26 20:01:03 -0500

    Setup TypeScript GraphQL with express & Apollo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import { buildSchema } from "type-graphql";
 import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -8,6 +11,19 @@ const main = async () => {
   await orm.getMigrator().up();
 
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
 };
 
 main().catch((err) => {
